44606a79bd479fe1d1d791404389c26bf4abe918
Brandon Purvis <bcp7cc@virginia.edu>
2015-06-22 00:00:31 -0400

    Display aggregation on usernames.

--- search/ask.py
+++ search/ask.py
@@ -6,5 +6,7 @@ def ask(query):
     response = settings.CLIENT.search(index=settings.ELASTIC_INDEX, doc_type='blame', body=base.build_query(query))
     hits = response['hits']['hits']
     aggs = response['aggregations']['per_user']['buckets']
+    for agg in aggs:
+        score = agg['doc_count']
     results = {'hits': hits, 'aggs': aggs}
     return results
